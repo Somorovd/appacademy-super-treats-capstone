56c3b4fcfa8802a025b6775c93bdc8509ee174b4
dsomorov <dsomorov@gmail.com>
2023-07-11 18:28:31 -0500

    update user menu position and add auth buttons

--- react-app/src/components/UserMenu/UserMenu.js
+++ react-app/src/components/UserMenu/UserMenu.js
@@ -1,20 +1,53 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { logout } from "../../store/session";
+import { useModal } from "../../context/Modal";
 
 import "./UserMenu.css";
 
 export default function UserMenu() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const handleSignOut = async () => {
+
+  const { closeModal } = useModal();
+  const user = useSelector((state) => state.session.user);
+
+  const modalNavigate = (path) => {
+    closeModal();
+    history.push(path);
+  };
+
+  const handleSignout = async () => {
     await dispatch(logout());
-    history.push("/");
+    modalNavigate("/");
   };
 
   return (
-    <div className="user-menu">
-      <button onClick={handleSignOut}>Sign out</button>
+    <div className="user-menu flex-c">
+      {user && (
+        <button
+          className="user-menu__signout"
+          onClick={handleSignout}
+        >
+          Sign out
+        </button>
+      )}
+      {!user && (
+        <>
+          <button
+            className="user-menu__login bt-black"
+            onClick={() => modalNavigate("/signup")}
+          >
+            Sign up
+          </button>
+          <button
+            className="user-menu__login"
+            onClick={() => modalNavigate("/login")}
+          >
+            Log in
+          </button>
+        </>
+      )}
     </div>
   );
 }
