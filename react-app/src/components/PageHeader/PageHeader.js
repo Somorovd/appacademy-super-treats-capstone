f52cf4aa8c8552b490aed93f862be2802d2f76c7
dsomorov <dsomorov@gmail.com>
2023-07-11 17:09:03 -0500

    remove auth buttons from header when logged in

--- react-app/src/components/PageHeader/PageHeader.js
+++ react-app/src/components/PageHeader/PageHeader.js
@@ -1,8 +1,12 @@
 import { useHistory } from "react-router-dom";
 import "./PageHeader.css";
+import { useSelector } from "react-redux";
 
 export default function PageHeader() {
   const history = useHistory();
+
+  const user = useSelector((state) => state.session.user);
+
   return (
     <header className="page-header flex flex-b1">
       <div className="header-left flex">
@@ -12,18 +16,22 @@ export default function PageHeader() {
         <div className="header-logo">SuperTreats</div>
       </div>
       <div className="header-right flex">
-        <button
-          className="header-login"
-          onClick={() => history.push("/login")}
-        >
-          <i className="fa-solid fa-user"></i> Log in
-        </button>
-        <button
-          className="header-signup bt-black"
-          onClick={() => history.push("/signup")}
-        >
-          Sign up
-        </button>
+        {!user && (
+          <>
+            <button
+              className="header-login"
+              onClick={() => history.push("/login")}
+            >
+              <i className="fa-solid fa-user"></i> Log in
+            </button>
+            <button
+              className="header-signup bt-black"
+              onClick={() => history.push("/signup")}
+            >
+              Sign up
+            </button>
+          </>
+        )}
       </div>
     </header>
   );
