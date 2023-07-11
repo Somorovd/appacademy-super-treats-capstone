920ffbfbc2c8f73e0881f585798dd157e3f76d5a
dsomorov <dsomorov@gmail.com>
2023-07-11 14:56:39 -0500

    create page header for landing page

--- react-app/src/components/PageHeader/PageHeader.js
+++ react-app/src/components/PageHeader/PageHeader.js
@@ -1,7 +1,30 @@
-import './PageHeader.css'
+import { useHistory } from "react-router-dom";
+import "./PageHeader.css";
 
 export default function PageHeader() {
+  const history = useHistory();
   return (
-    <h1> PageHeader </h1>
-  )
+    <header className="page-header flex flex-b1">
+      <div className="header-left flex">
+        <div className="header-menu">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+        <div className="header-logo">SuperTreats</div>
+      </div>
+      <div className="header-right flex">
+        <button
+          className="header-login"
+          onClick={() => history.push("/login")}
+        >
+          <i class="fa-solid fa-user"></i> Log in
+        </button>
+        <button
+          className="header-signup bt-black"
+          onClick={() => history.push("/signup")}
+        >
+          Sign up
+        </button>
+      </div>
+    </header>
+  );
 }
