5212f85571121a1f934d33427bf9f7ed7751e6c4
dsomorov <dsomorov@gmail.com>
2023-07-12 15:31:53 -0500

    create new business form

--- react-app/src/components/UserBusinessPage/CreateBusinessForm/CreateBusinessForm.js
+++ react-app/src/components/UserBusinessPage/CreateBusinessForm/CreateBusinessForm.js
@@ -1,5 +1,133 @@
+import { useState } from "react";
+
 import "./CreateBusinessForm.css";
 
 export default function CreateBusinessForm() {
-  return <h1> CreateBusinessForm </h1>;
+  const [address, setAddress] = useState("");
+  const [businessName, setBusinessName] = useState("");
+  const [brandName, setBrandName] = useState("");
+  const [type, setType] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+  };
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (address.trim() === "") errors.address = "Enter a store address";
+    if (address.trim().length > 255)
+      errors.address = "The maximum characters allowed is 255";
+
+    if (businessName.trim() === "") errors.name = "Enter a store name";
+    if (businessName.trim().length > 255)
+      errors.address = "The maximum characters allowed is 255";
+
+    if (brandName.trim() === "") errors.brand = "Enter a brand name";
+    if (brandName.trim().length > 255)
+      errors.address = "The maximum characters allowed is 255";
+
+    if (type === "") errors.type = "Select a business type";
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  return (
+    <div className="create-business-page flex-c">
+      <form
+        className="create-business-form"
+        onSubmit={handleSubmit}
+      >
+        <h2>Get Started</h2>
+
+        <div>
+          <label htmlFor="business-address">Store Address</label>
+          <input
+            id="business-address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <p className="auth-error">{errors.address}</p>
+        </div>
+
+        <div>
+          <label htmlFor="business-name">Store Name</label>
+          <input
+            id="business-name"
+            placeholder="Example: App Academy - Feb. Online"
+            value={businessName}
+            onChange={(e) => setBusinessName(e.target.value)}
+          />
+          {errors.name ? (
+            <p className="auth-error">{errors.name}</p>
+          ) : (
+            <p>This is how your store will appear in the app.</p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="brand-name">Brand Name</label>
+          <input
+            id="brand-name"
+            placeholder="Example: App Academy"
+            value={brandName}
+            onChange={(e) => setBrandName(e.target.value)}
+          />
+          {errors.brand ? (
+            <p className="auth-error">{errors.brand}</p>
+          ) : (
+            <p>
+              We'll use this to help organize information that is shared across
+              stores, such as menus.
+            </p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="business-type">Business Type</label>
+          <select
+            id="business-type"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option
+              value=""
+              hidden
+            >
+              Select...
+            </option>
+            <option>Restaurant</option>
+            <option>Grocery Store</option>
+          </select>
+          <p className="auth-error">{errors.type}</p>
+        </div>
+
+        {type === "Restaurant" && (
+          <div>
+            <label htmlFor="cuisine">Cuisine</label>
+            <select
+              id="cuisine"
+              value={cuisine}
+              onChange={(e) => setCuisine(e.target.value)}
+            >
+              <option
+                value=""
+                hidden
+              >
+                Select...
+              </option>
+              <option>BBQ</option>
+              <option>Chinese</option>
+            </select>
+            <p className="auth-error">{errors.cuisine}</p>
+          </div>
+        )}
+        <button className="bt-black">Submit</button>
+      </form>
+    </div>
+  );
 }
