bff868b543b6d08f9792e1eddd176bc21e13ddc6
dsomorov <dsomorov@gmail.com>
2023-07-12 16:02:20 -0500

    create new business wtform

---
+++ app/forms/create_business_form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length, AnyOf
+from ..models.business import types, cuisines, price_ranges
+
+
+class LoginForm(FlaskForm):
+    address = StringField("address", validators=[DataRequired(), Length(1, 255)])
+    name = StringField("address", validators=[DataRequired(), Length(1, 100)])
+    price_range = StringField(
+        "price_range",
+        validators=[DataRequired(), AnyOf([e.name for e in price_ranges])],
+    )
+    delivery_fee = StringField("delivery", validators=[DataRequired()])
+    type = (
+        StringField(
+            "type", validators=[DataRequired(), AnyOf([e.name for e in types])]
+        ),
+    )
+    cuisine = StringField(
+        "cuisine", validators=[DataRequired(), AnyOf([e.name for e in cuisines])]
+    )
