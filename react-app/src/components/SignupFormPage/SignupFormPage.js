618cac5c416dd8ab477058a7942d9c3299e59d9e
dsomorov <dsomorov@gmail.com>
2023-07-11 19:09:30 -0500

    add signup form input fields

--- react-app/src/components/SignupFormPage/SignupFormPage.js
+++ react-app/src/components/SignupFormPage/SignupFormPage.js
@@ -8,7 +8,9 @@ export default function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  const [phone, setPhone] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
@@ -17,65 +19,116 @@ export default function SignupFormPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data);
-      }
-    } else {
-      setErrors([
-        "Confirm Password field must be the same as the Password field",
-      ]);
-    }
+    // if (password === confirmPassword) {
+    //   const data = await dispatch(signUp(username, email, password));
+    //   if (data) {
+    //     setErrors(data);
+    //   }
+    // } else {
+    //   setErrors([
+    //     "Confirm Password field must be the same as the Password field",
+    //   ]);
+    // }
+  };
+
+  const validateMaxLength = (field, val, len, setter) => {
+    const error = val.length > len ? "Max length reached" : "";
+    setErrors((errors) => ({ ...errors, [`${field}`]: error }));
+
+    if (error) return;
+    else setter(val);
   };
 
   return (
-    <>
+    <div className="signup-page fh">
       <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
-    </>
+      <div className="signup-content flex flex-11 fh">
+        <form
+          className="signup-form"
+          onSubmit={handleSubmit}
+        >
+          <section className="signup-form__section">
+            <h2>What is your email address?</h2>
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              value={email}
+              onChange={(e) =>
+                validateMaxLength("email", e.target.value, 255, setEmail)
+              }
+              required
+            />
+            <p className="auth-error">{errors.email}</p>
+          </section>
+
+          <section className="signup-form__section">
+            <h2>What is your phone number?</h2>
+            <input
+              type="text"
+              placeholder="Enter your phone number"
+              pattern=""
+              value={phone}
+              onChange={(e) =>
+                validateMaxLength("phone", e.target.value, 20, setPhone)
+              }
+              required
+            />
+            <p className="auth-error">{errors.phone}</p>
+          </section>
+
+          <section className="signup-form__section">
+            <h2>Create your account password</h2>
+            <p>
+              Your password must be at least 8 characters long and contain at
+              least one letter and one digit
+            </p>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) =>
+                validateMaxLength("password", e.target.value, 20, setPassword)
+              }
+              minLength={8}
+              required
+            />
+            <p className="auth-error">{errors.password}</p>
+          </section>
+
+          <section className="signup-form__section">
+            <h2>What is your name?</h2>
+            <p>Let us know how to properly address you</p>
+            <input
+              type="text"
+              placeholder="Enter first name"
+              value={firstName}
+              onChange={(e) =>
+                validateMaxLength(
+                  "firstName",
+                  e.target.value,
+                  255,
+                  setFirstName
+                )
+              }
+              required
+            />
+            <p className="auth-error">{errors.firstName}</p>
+
+            <input
+              type="text"
+              placeholder="Enter last name"
+              value={lastName}
+              onChange={(e) =>
+                validateMaxLength("lastName", e.target.value, 255, setLastName)
+              }
+              required
+            />
+            <p className="auth-error">{errors.lastName}</p>
+          </section>
+
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
   );
 }
