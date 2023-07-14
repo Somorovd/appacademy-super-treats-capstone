a242177e474d4316cd34f314053ba3cdc5fbe168
dsomorov <dsomorov@gmail.com>
2023-07-14 11:55:10 -0500

    update auth route validation

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, Length, ValidationError
 from app.models import User
 from ..utils.helpers import get_phone_from_input
 
@@ -22,15 +22,7 @@ def phone_exists(form, field):
     phone = get_phone_from_input(field.data)
     user = User.query.filter(User.phone == phone).first()
     if user:
-        raise ValidationError("Phone address is already in use.")
-
-
-def username_exists(form, field):
-    # Checking if username is already in use
-    username = field.data
-    user = User.query.filter(User.username == username).first()
-    if user:
-        raise ValidationError("Username is already in use.")
+        raise ValidationError("Phone number is already in use.")
 
 
 class SignupEmailForm(FlaskForm):
@@ -44,6 +36,6 @@ class SignupPhoneForm(FlaskForm):
 class SignUpForm(FlaskForm):
     email = StringField("email", validators=[DataRequired(), Email(), email_exists])
     phone = StringField("phone", validators=[DataRequired(), valid_phone, phone_exists])
-    password = StringField("password", validators=[DataRequired()])
-    first_name = StringField("password", validators=[DataRequired()])
-    last_name = StringField("password", validators=[DataRequired()])
+    password = StringField("password", validators=[DataRequired(), Length(8, 40)])
+    first_name = StringField("password", validators=[DataRequired(), Length(1, 255)])
+    last_name = StringField("password", validators=[DataRequired(), Length(1, 255)])
