ef3224d5fb3d85c5c6e78bf901ec23f82c22c184
dsomorov <dsomorov@gmail.com>
2023-07-14 16:55:59 -0500

    protect unauthorized deep business routes

--- react-app/src/components/BusinessRouter/UserBusinessManagementPage.js
+++ react-app/src/components/BusinessRouter/UserBusinessManagementPage.js
@@ -1,18 +1,36 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useParams, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import BusinessMenu from "./BusinessMenu";
-import CreateItemForm from "./CreateItemForm";
+import ItemEditPage from "./ItemEditPage";
 import BusinessInfoPage from "./BusinessInfoPage";
+import { thunkGetOneBusiness } from "../../store/userBusinesses";
 
 export default function UserBusinessManagementPage() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { businessId } = useParams();
+
+  const business = useSelector((state) => state.userBusinesses.singleBusiness);
+
+  useEffect(() => {
+    (async () => {
+      const res = await dispatch(thunkGetOneBusiness(businessId));
+      if (res.errors) history.push("/business");
+    })();
+  }, [dispatch]);
+
+  if (business.id !== Number(businessId)) return null;
+
   return (
     <div className="business-page">
       <BusinessMenu />
       <Switch>
         <Route path="/business/:businessId/items/new">
-          <CreateItemForm />
+          <ItemEditPage />
         </Route>
         <Route path="/business/:businessId/items/:itemId">
-          <CreateItemForm />
+          <ItemEditPage />
         </Route>
         <Route path="/business/:businessId">
           <BusinessInfoPage />
