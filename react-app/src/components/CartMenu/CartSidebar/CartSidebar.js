db295cefbe27bdc3a8444032b2cd7bfbc164c65b
dsomorov <dsomorov@gmail.com>
2023-07-18 11:36:10 -0500

    create ItemBrowsing and CartSidebar components

---
+++ react-app/src/components/CartMenu/CartSidebar/CartSidebar.js
@@ -0,0 +1,7 @@
+import './CartSidebar.css'
+
+export default function CartSidebar() {
+  return (
+    <h1> CartSidebar </h1>
+  )
+}
---
+++ react-app/src/components/ItemBrowsingPage/ItemBrowsingPage.js
@@ -0,0 +1,7 @@
+import './ItemBrowsingPage.css'
+
+export default function ItemBrowsingPage() {
+  return (
+    <h1> ItemBrowsingPage </h1>
+  )
+}
