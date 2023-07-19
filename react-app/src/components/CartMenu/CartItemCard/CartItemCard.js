68aa247702d89d96a99b2e20a97ea875915a601b
dsomorov <dsomorov@gmail.com>
2023-07-18 19:01:22 -0500

    add info and styling to cart item cards

--- react-app/src/components/CartMenu/CartItemCard/CartItemCard.js
+++ react-app/src/components/CartMenu/CartItemCard/CartItemCard.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import "./CartItemCard.css";
 
 export default function CartItemCard({ cartItem }) {
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
+  const handleChangeQuantity = (e) => {
+    setQuantity(e.target.value);
+  };
+
   return (
-    <div className="cart-item-card">
-      <p>{cartItem.item.name}</p>
+    <div className="cart-item-card flex-c">
+      <div className="flex flex-b1">
+        <p>{cartItem.item.name}</p>
+        <img
+          src={cartItem.item.image}
+          alt=""
+        />
+      </div>
+      <div className="flex flex-b1">
+        <select
+          value={quantity}
+          onChange={handleChangeQuantity}
+          className="quantity-select"
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+        <span>${cartItem.price}</span>
+      </div>
     </div>
   );
 }
