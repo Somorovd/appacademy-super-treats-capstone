3d34dcee84f8985240c974f79d961070471f0838
dsomorov <dsomorov@gmail.com>
2023-07-19 17:27:20 -0500

    remove catogories text

--- react-app/src/components/ItemBrowsingPage/CategorySidebar/CategorySidebar.js
+++ react-app/src/components/ItemBrowsingPage/CategorySidebar/CategorySidebar.js
@@ -1,9 +1,5 @@
 import "./CategorySidebar.css";
 
 export default function CategorySidebar() {
-  return (
-    <div className="category-sidebar">
-      <h2>Categories!</h2>
-    </div>
-  );
+  return <div className="category-sidebar"></div>;
 }
