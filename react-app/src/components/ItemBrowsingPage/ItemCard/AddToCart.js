fe90eea4d6f8e8088f230e150156023c05b21352
dsomorov <dsomorov@gmail.com>
2023-07-20 13:20:35 -0500

    extracting add to cart component

---
+++ react-app/src/components/ItemBrowsingPage/ItemCard/AddToCart.js
@@ -0,0 +1,71 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+
+import { thunkAddToCart } from "../../../store/carts";
+
+export default function AddToCart({ item }) {
+  const dispatch = useDispatch();
+  const menuRef = useRef();
+  const { businessId } = useParams();
+  const [expand, setExpand] = useState(false);
+
+  const cartItem = useSelector(
+    (state) => state.carts[businessId]?.cartItems[item.id]
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
+  }, []);
+
+  const addToCart = (e) => {
+    e.stopPropagation();
+    const itemObj = {
+      item_id: item.id,
+      quantity: 1,
+      address: sessionStorage.getItem("current-address"),
+    };
+    dispatch(thunkAddToCart(itemObj));
+  };
+
+  const closeMenu = () => {
+    setExpand(false);
+  };
+
+  const openMenu = (e) => {
+    e.stopPropagation();
+    if (!expand) setExpand(true);
+    else closeMenu();
+  };
+
+  return (
+    <>
+      <div
+        className="add-to-cart flex flex-11"
+        ref={menuRef}
+        onClick={openMenu}
+      >
+        {expand && (
+          <button
+            className="bt-black flex flex-11"
+            // onClick={addToCart}
+          >
+            <i className="fa-solid fa-trash"></i>
+          </button>
+        )}
+        {cartItem && (
+          <span className="add-to-cart__count">{cartItem.quantity}</span>
+        )}
+        {(!cartItem || expand) && (
+          <button
+            className="bt-black flex flex-11"
+            onClick={addToCart}
+          >
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
