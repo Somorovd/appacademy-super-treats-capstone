64e5b6941a6699fa0e2a62bb9910259e69b4ea19
dsomorov <dsomorov@gmail.com>
2023-09-13 12:52:35 -0500

    add 404 page

--- react-app/src/components/Page404/Page404.js
+++ react-app/src/components/Page404/Page404.js
@@ -1,7 +1,13 @@
-import './Page404.css'
+import "./Page404.css";
 
 export default function Page404() {
   return (
-    <h1> Page404 </h1>
-  )
+    <div
+      id="404-page"
+      className="fw fh flex-c flex-11"
+    >
+      <h1>404 Page Not Found</h1>
+      <p>The page you are trying to reach does not exist</p>
+    </div>
+  );
 }
